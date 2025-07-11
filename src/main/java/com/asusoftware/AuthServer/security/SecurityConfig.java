9a8d34096d627bc1f81fc7a2f26fdc550e426d20
antonio <antonio.popa.dev@gmail.com>
2025-07-11 14:44:13 +0300

    Fix

--- src/main/java/com/asusoftware/AuthServer/security/SecurityConfig.java
+++ src/main/java/com/asusoftware/AuthServer/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
